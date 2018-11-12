ffaa26b33744a984c7d5f47e5d9fba5ff8077272
Noah Theodore Prisament <nprisament@gmail.com>
2018-11-11 23:58:12 -0500

    start

--- Project3/snake.js
+++ Project3/snake.js
@@ -1,12 +1,12 @@
 const WIDTH = 100, HEIGHT = 100, size = 4;
 let grid, isStart = false, isPaused = false;
-let snake, length = 5, xDot, yDot;
-
+let snake, length = 5, xDot, yDot, direction = 'right';
 
 function setup() { 
 	grid = new Array(HEIGHT).fill(new Array(WIDTH).fill(0));
 	snake = new Array(2).fill(new Array(1).fill(WIDTH / 2));
 	//load the grid
+	noStroke();
 	createCanvas(400, 400);
 } 
 
@@ -19,4 +19,29 @@ function draw() {
 	//move snake
 	//check for collision
 	//print snake and board
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 74:
+      if (direction != 'right') {
+        direction = 'left';
+      }
+      break;
+    case 76:
+      if (direction != 'left') {
+        direction = 'right';
+      }
+      break;
+    case 73:
+      if (direction != 'down') {
+        direction = 'up';
+      }
+      break;
+    case 75:
+      if (direction != 'up') {
+        direction = 'down';
+      }
+      break;
+  }
 }
