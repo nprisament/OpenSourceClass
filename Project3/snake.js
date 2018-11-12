a9a08ab8351f9d4dcb80a65247ee955eecefa5fa
Noah Theodore Prisament <nprisament@gmail.com>
2018-11-11 23:41:37 -0500

    make layout

--- Project3/snake.js
+++ Project3/snake.js
@@ -1,7 +1,22 @@
+const WIDTH = 100, HEIGHT = 100, size = 4;
+let grid, isStart = false, isPaused = false;
+let snake, length = 5, xDot, yDot;
+
+
 function setup() { 
-  createCanvas(400, 400);
+	grid = new Array(HEIGHT).fill(new Array(WIDTH).fill(0));
+	snake = new Array(2).fill(new Array(1).fill(WIDTH / 2));
+	//load the grid
+	createCanvas(400, 400);
 } 
 
-function draw() { 
-  background(220);
+function draw() {
+	//if start screen
+	//if paused, continue
+
+	//find the keyboard input
+	background(220);
+	//move snake
+	//check for collision
+	//print snake and board
 }
