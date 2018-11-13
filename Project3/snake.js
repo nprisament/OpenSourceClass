d99dd8224fcd8b263122a09b7823dec71e8dc3dc
Noah Theodore Prisament <nprisament@gmail.com>
2018-11-13 17:34:10 -0500

    pause and game over

--- Project3/snake.js
+++ Project3/snake.js
@@ -1,6 +1,7 @@
 const WIDTH = 70, HEIGHT = 70, size = 10;
 let grid, isStart = true, pause = false, party = false, over = false, score, scores = [];
 let xSnake, ySnake, length, xDot, yDot, lastDirection, direction;
+mod = (num) => num * size + floor(size / 2);
 
 function setup() {
 	createCanvas(WIDTH * size, HEIGHT * size);
@@ -10,41 +11,49 @@ function setup() {
 	init();
 }
 
+function init() {
+  grid = new Array(HEIGHT).fill(new Array(WIDTH).fill(false));
+  xSnake = new Array(1).fill(floor(WIDTH / 2));
+  ySnake = new Array(1).fill(floor(HEIGHT / 2));
+  score = -1;
+  length = 2;
+  pause = false;
+  lastDirection = direction = 'right';
+  eatDot();
+  //load the grid
+}
+
 function draw() {
 	if (over) {
-		gameOverDraw();
-		return;
+    strokeWeight(0);
+    fill(0);
+    textSize(size * 3);
+    text('Game Over, Score ' + scores[scores.length - 1], mod(WIDTH / 5), mod(HEIGHT / 2));
+    text('Press 1 to Start a New Game', mod(WIDTH / 5), mod(5 * HEIGHT / 8));
+    return;
 	}
 	if (isStart) {
 		startDraw();
 		return;
 	}
 	if (pause) {
-		pauseDraw();
-		return;
+		strokeWeight(0);
+    fill(0);
+    textSize(size * 4);
+    text('Paused', mod(WIDTH / 3), mod(HEIGHT / 2));
+    return;
 	}
 	background(100);
 	if (party) partyDraw();
 	moveSnake();
-	strokeWeight(size);
-	snakeDraw();
+  //game elements
+  snakeDraw();
 	gridDraw();
-	fill(255);
-  	strokeWeight(0);
+  //score
+	fill(200);
+  strokeWeight(0);
 	textSize(size*2);
 	text('Score: ' + score, 10, 30);
-}
-
-function init() {
-	grid = new Array(HEIGHT).fill(new Array(WIDTH).fill(false));
-	xSnake = new Array(1).fill(floor(WIDTH / 2));
-	ySnake = new Array(1).fill(floor(HEIGHT / 2));
-	score = -1;
-	length = 2;
-	pause = false;
-	lastDirection = direction = 'right';
-	eatDot();
-	//load the grid
 }
 
 function moveSnake() {
@@ -91,48 +100,62 @@ function moveSnake() {
 	lastDirection = direction;
 }
 
+function testPoint(x, y) {
+  if (grid[x][y]) return false;
+  let inSnake = false;
+  for (let i = 0; i < xSnake.length; i++) {
+    if (xSnake[i] == x && ySnake[i] == y)
+      inSnake = true;
+  }
+  return !inSnake;  
+}
+
 function eatDot() {
 	score++;
-	length += 3;
+	length += 3 + (score > 5 ? score - 5 : 0) ;
 	let makingDot = true;
 	while(makingDot) {
 		xDot = floor(random(10, WIDTH - 10));
 		yDot = floor(random(10, HEIGHT - 10));
-		if (grid[xDot][yDot]) continue;
-		let inSnake = false;
-		for (let i = 0; i < xSnake.length; i++) {
-			if (xSnake[i] == xDot && ySnake[i] == yDot)
-				inSnake = true;
-		}
-		if (inSnake) continue;
-		makingDot = false;
+		makingDot = !testPoint(xDot, yDot);
 	}
 	addBarrier();
 }
 
 function addBarrier() {
-//later
+  let makingBarrier = true;
+  let xB, yB;
+  while(makingBarrier) {
+    xB = floor(random(0, WIDTH));
+    yB = floor(random(0, HEIGHT));  
+    makingBarrier = !textPoint(xB, yB);
+  }
+  grid[xB][yB] = true;
 }
 
 function gameOver() {
 	scores.push(score);
 	init();
 	over = true;
-	isStart = true;
 }
 
-mod = (num) => num * size + floor(size / 2);
-
 function snakeDraw() {
-	for (let i = 0; i < xSnake.length - 1; i++)
+  strokeWeight(size);
+	stroke(255);
+  for (let i = 0; i < xSnake.length - 1; i++)
 		line(mod(xSnake[i]), mod(ySnake[i]), mod(xSnake[i + 1]), mod(ySnake[i + 1]));
 }
 
 function gridDraw() {
-	for (let i = 0; i < grid.length; i++) {
+	strokeWeight(size);
+  stroke(0, 0, 180);
+  //draw barriers
+  for (let i = 0; i < grid.length; i++) {
 		for (let j = 0; j < grid[i].length; j++)
 			if (grid[i][j]) line(mod(i), mod(j), mod(i), mod(j));
 	}
+  //draw dot
+  stroke(180,0,0);
 	line(mod(xDot), mod(yDot), mod(xDot), mod(yDot));
 }
 
@@ -141,17 +164,6 @@ function startDraw() {
 	//some design
 	//name
 	//how to start
-}
-
-function gameOverDraw() {
-	background(0);
-	//draw a game over state,
-	//make it clickable to go back to start menu	
-}
-
-function pauseDraw() {
-	textSize(size * 4);
-	text('Paused', mod(WIDTH / 4), mod(HEIGHT / 2));
 }
 
 function partyDraw() {
